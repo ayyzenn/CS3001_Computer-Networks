dc6e2b6512df53af17154be3322bedcfd98f9bd0
Saad Ahmad <saad.ahmad1024@gmail.com>
2024-01-27 17:24:12 +0500

    another one....

---
+++ Misc/socket_prog/client.py
@@ -0,0 +1,13 @@
+# echo-client.py
+
+import socket
+
+host = "127.0.0.1"  # The server's hostname or IP address
+port = 65432  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((host, port))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+
+print(f"Received {data!r}")
